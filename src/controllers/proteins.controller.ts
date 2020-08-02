047ff7f72c3137acd90f2ddb97ca793c5cdd2418
Gabriel <teixeira.gabriel93@gmail.com>
2020-08-02 14:29:19 -0300

    handling errors

--- src/controllers/proteins.controller.ts
+++ src/controllers/proteins.controller.ts
@@ -3,38 +3,56 @@ import query from '../config/database';
 import {
     httpErrorHelper,
     httpResponseHelper
-} from '../helpers/index'
-
-
-//SELECT protein_accession, description, label, synonyms FROM protein_info;'
+} from '../helpers/index';
 
 export const listAllProteins = async (req: Request, res: Response) => {
 
-    let limit: any = req.query.limit;
-    let page: any = req.query.page;
-    let offset = (page - 1) * limit;
+    try {
+        let limit: any = req.query.limit;
+        let page: any = req.query.page;
+        let offset = (page - 1) * limit;
+    
+        const response = await query('SELECT protein_accession, description, label, synonyms FROM protein_info LIMIT $1 OFFSET $2;', [limit, offset]);
 
-    const response = await query('SELECT protein_accession, description, label, synonyms FROM protein_info LIMIT $1 OFFSET $2;', [limit, offset]);
-    console.log(response.rowCount)
-    res.status(200).json(httpResponseHelper(
-        response.rows,
-        Number(req.query.page)
-    ));
+        if(response.rowCount === 0) 
+            return res.status(400).json(httpErrorHelper({
+                code: 400,
+                message: 'NULL DATA'
+            }));
+    
+        return res.status(200).json(httpResponseHelper(
+            response.rows,
+            Number(req.query.page)
+        ));
+    }
+    catch (err) {
+        return res.status(400).json(httpErrorHelper({
+            code: 400,
+            message: err.name
+        }));
+    }
 }
 
 export const listProteinById = async (req: Request, res: Response) => {
 
-    const proteinId = req.params.id;
-    const response = await query('SELECT protein_accession, description, label, synonyms FROM protein_info WHERE protein_accession = $1', [proteinId]);
-
-    if(response.rowCount === 0) 
-        return res.status(404).json(httpErrorHelper({
-            code: 404,
-            message: 'ID NOT FOUND'
-        }));
-        
-    return res.status(200).json(httpResponseHelper(
-        response.rows
-    ));
+    try {
+        const proteinId = req.params.id;
+        const response = await query('SELECT protein_accession, description, label, synonyms FROM protein_info WHERE protein_accession = $1', [proteinId]);
     
+        if(response.rowCount === 0) 
+            return res.status(404).json(httpErrorHelper({
+                code: 404,
+                message: 'ID NOT FOUND'
+            }));
+            
+        return res.status(200).json(httpResponseHelper(
+            response.rows
+        ));
+    } 
+    catch(err) {
+        return res.status(400).json(httpErrorHelper({
+            code: 400,
+            message: err.name
+        }));
+    } 
 }
